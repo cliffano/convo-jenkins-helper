769bc9b245e2f22ae8d5a7f3aca78da69eedf5b1
Cliffano Subagio <cliffano@gmail.com>
2018-09-10 01:03:38 +1000

    Add placeholder test.

---
+++ test/args/job.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const job = require('../../lib/args/job')
+
+describe('job', function() {
+  describe('postJobBuild', function() {
+    it('should TODO', function() {
+      // TODO
+    });
+  });
+});
